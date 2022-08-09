3c2e035a4515170a21534861b16d533b3b6904d6
BooDeer <hsissi3a@gmail.com>
2022-08-10 00:42:26 +0100

    Added instagram

--- facebookless/facebookless.js
+++ facebookless/facebookless.js
@@ -1,12 +1,20 @@
 const storage = chrome.storage.sync;
 
-if (window.location.hostname.includes("facebook"))
-{
+if (window.location.hostname.includes("facebook")) {
 	storage.get("facebook", (e) => {
 		if (Object.values(e)[0] === true){
 			const target = document.querySelector('[role="main"]')
-			setTimeout(null, 3000);
+			// setTimeout(null, 3000);
 			target.parentNode.removeChild(target);
+		}
+	})
+} else if (window.location.hostname.includes("instagram")) {
+	storage.get("instagram", (e) => {
+		if (Object.values(e)[0] === true){
+			setTimeout(() => {
+				const target = document.querySelector('main')
+				t	arget.parentNode.removeChild(target);
+			}, 1100);
 		}
 	})
 }
--- facebookless/options/options.js
+++ facebookless/options/options.js
@@ -2,11 +2,8 @@ const storage = chrome.storage.sync;
 
 //* check the checked checkboxes checked previously by the checkboxer when he checked the checkboxes.
 (() => {
-	console.log("===> initial function")
 	document.querySelectorAll('input').forEach(elem => {
 		storage.get(elem.id, (e) => {
-			console.log(e)
-			console.log(Object.values(e)[0])
 			if (Object.values(e)[0] === true)
 			{
 				elem.checked = true;
