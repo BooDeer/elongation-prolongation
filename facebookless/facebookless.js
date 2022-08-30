08f8260be697a3144bdd1b96cbb3e63bd51dd14a
BooDeer <hsissi3a@gmail.com>
2022-08-30 17:56:56 +0100

    Added more websites

--- facebookless/facebookless.js
+++ facebookless/facebookless.js
@@ -1,5 +1,11 @@
 const storage = chrome.storage.sync;
 
+
+//* Output the name of the current website. (for debugging)
+// console.log(window.location.hostname)
+
+
+
 if (window.location.hostname.includes("facebook")) {
 	storage.get("facebook", (e) => {
 		if (Object.values(e)[0] === true){
@@ -13,8 +19,31 @@ if (window.location.hostname.includes("facebook")) {
 		if (Object.values(e)[0] === true){
 			setTimeout(() => {
 				const target = document.querySelector('main')
-				t	arget.parentNode.removeChild(target);
+				target.parentNode.removeChild(target);
 			}, 1100);
 		}
 	})
-}
+} else if (window.location.hostname.includes("twitter")) {
+	storage.get("twitter", (e) => {
+		if (Object.values(e)[0] === true){
+			setTimeout(() => {
+				const target = document.querySelector('[role="region"]')
+				//TODO: should I delete the trend border too?
+				// console.log(target.firstChild)
+				target.parentNode.removeChild(target);
+			}, 3000);
+			//! Sadly it takes a lot of time to render this element thus the long timeout
+		}
+})} else if (window.location.hostname.includes("reddit")) {
+	storage.get("reddit", (e) => {
+		if (Object.values(e)[0] === true){
+			setTimeout(() => {
+				const target = document.querySelectorAll('[style="max-width:100%"')
+				// for (let i = 0; i < target.length; i++)
+				target[1].removeChild(target[1].firstChild);
+				// for (let e in target)
+					// e.parentNode.removeChild(e);
+				// target.parentNode.removeChild(target);
+			}, 500);
+		}
+})}
